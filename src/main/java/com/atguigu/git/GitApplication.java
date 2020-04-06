a225cd2a17d80b5f9396fa13e59ff6a381b8b304
fky_spring <fky-spring@163.com>
2020-04-06 22:51:01 +0800

    firstcommit

---
+++ src/main/java/com/atguigu/git/GitApplication.java
@@ -0,0 +1,18 @@
+package com.atguigu.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class GitApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(GitApplication.class, args);
+    }
+
+
+
+}
